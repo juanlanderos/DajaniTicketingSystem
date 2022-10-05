30622a469385f8305028c44072728e91ece55f38
Russellyee <98377506+Russellyee@users.noreply.github.com>
2022-10-04 22:18:10 -0700

    DTS-166 filter and bug fixing

--- frontend-app/src/app/Services/ticket.service.ts
+++ frontend-app/src/app/Services/ticket.service.ts
@@ -12,8 +12,14 @@ export class TicketService {
 
   constructor(private httpClient: HttpClient) { }
 
+  /* //Commented until db is up and running.
   getTicketsList(): Observable<Ticket[]>{
     return this.httpClient.get<Ticket[]>(this.baseUrl);
+  }
+  */
+  //Used as 'dummy' db file. REMOVE/REPLACE WHEN DB IS CONNECTED AND RUNNING!
+  getTicketsList(): Observable<any>{
+    return this.httpClient.get('/assets/ticket_data.json');
   }
 
   createTicket(ticket: Ticket): Observable<Object>{
