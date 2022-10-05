b7eff43e4ca89023d60d97999fee0627c434a290
djbeauchamp-csus <80233465+djbeauchamp-csus@users.noreply.github.com>
2022-10-04 21:22:34 -0700

    DTS-154 > added sidenav with buttons, linked the view all tickets since that is all I had to link to at the moment.

--- frontend-app/src/app/app.module.ts
+++ frontend-app/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 //Components below
 import { UserListComponent } from './Components/user-list/user-list.component';
 import { CreateUserComponent } from './Components/create-user/create-user.component';
@@ -12,6 +12,14 @@ import { TicketListComponent } from './Components/ticket-list/ticket-list.compon
 import { CreateTicketComponent } from './Components/create-ticket/create-ticket.component';
 import { CommentListComponent } from './Components/comment-list/comment-list.component';
 import { CreateCommentComponent } from './Components/create-comment/create-comment.component';
+
+//Daniel Imports Below this
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+
+//Daniel Imports Above this
 
 @NgModule({
   declarations: [
@@ -29,6 +37,12 @@ import { CreateCommentComponent } from './Components/create-comment/create-comme
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    //Daniel Imports below here
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    //Daniel Imports above here
   ],
   providers: [],
   bootstrap: [AppComponent]
