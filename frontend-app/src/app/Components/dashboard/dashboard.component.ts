959f9006e27a0b34195bc176e0b6d3ce603d6be0
juanlanderos <juanlanderos@csus.edu>
2022-11-11 15:56:44 -0800

    Update dashboard.component.ts

--- frontend-app/src/app/Components/dashboard/dashboard.component.ts
+++ frontend-app/src/app/Components/dashboard/dashboard.component.ts
@@ -31,35 +31,12 @@ export class DashboardComponent implements OnInit {
 			Since login is not working properly, I first check if there is a token that exists: 
 			if there is, pick that user data from there. else, added a dummy user
 		*/
-<<<<<<< Updated upstream
-
-		const token = this.localStorage.get("access_token");
-
-		if (token) {
-			let decodeToken: any = jwt_decode(token);
-
-			this.user = {
-				userId: 1,
-				username: decodeToken.sub,
-				email: '',
-				commentEntityList: [],
-				firstName: '',
-				lastName: '',
-				roles: decodeToken.roles,
-				ticketEntities: []
-			}
-		}
-
-		// Get all the tickets associated with current logged in user.
-		this.getCurrentUserTickets();
-=======
 		this.username = this.localStorage.get("current_user") as string;
 		this.userService.getUserByUsername(this.username).subscribe(data => {
 			this.user = data;
 			console.log(this.user);
 			this.getCurrentUserTickets();
 		});
->>>>>>> Stashed changes
 	}
 
 	getCurrentUserTickets(): void {
@@ -67,28 +44,5 @@ export class DashboardComponent implements OnInit {
 			this.tickets = data;
 			console.log(this.tickets);
 		});
-<<<<<<< Updated upstream
-
-		// Remove this when above API starts working properly
-		/*this.tickets = [
-			{ 	ticketId: 1, assigneeId: 1, 
-				commentList: [], requesterId: 1,
-				statusId: 'Open', ticketDept: '',
-				title: 'Test Ticket 01', userList: [],
-				completedAt: new Date().toLocaleString(), 
-				createdAt: new Date().toLocaleString(),
-				updatedAt: new Date().toLocaleString()
-			},
-			{ 	ticketId: 2, assigneeId: 1, 
-				commentList: [], requesterId: 1,
-				statusId: 'Open', ticketDept: '',
-				title: 'Test Ticket 02', userList: [],
-				completedAt: new Date().toLocaleString(), 
-				createdAt: new Date().toLocaleString(),
-				updatedAt: new Date().toLocaleString()
-			} 
-		] */
-=======
->>>>>>> Stashed changes
 	}
 }
